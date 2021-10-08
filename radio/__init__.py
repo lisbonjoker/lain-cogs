054cd71c562e4babd458a2b0b319278f1c3e005b
Artur Mancha <joker@nadaradical.com>
2021-10-08 14:29:17 +0200

    Update __init__.py

--- radio/__init__.py
+++ radio/__init__.py
@@ -3,6 +3,8 @@ import asyncio
 import listenmoe
 import logging
 
+__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
+
 # from monstercatFM import monstercat
 from redbot.core import commands
 
